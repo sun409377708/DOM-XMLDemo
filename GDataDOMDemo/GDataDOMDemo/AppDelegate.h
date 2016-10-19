40e6edd77c81bdd626686eb1b8ce9a1e3f44870e
daling <sun409377708@163.com>
2016-10-19 09:45:23 +0800

    创建项目

---
+++ GDataDOMDemo/GDataDOMDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GDataDOMDemo
+//
+//  Created by maoge on 16/10/19.
+//  Copyright © 2016年 maoge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GDataDOMDemo/GDataDOMDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GDataDOMDemo
+//
+//  Created by maoge on 16/10/19.
+//  Copyright © 2016年 maoge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
